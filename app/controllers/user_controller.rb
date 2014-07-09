8809b824044d99af18dabb44473c9c00b979f02a
Kevin Casey <kcasey@godaddy.com>
2014-07-09 14:03:08 -0700

    omg

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,6 @@
-class WelcomeController < ApplicationController
-  def index
+class UserController < ApplicationController
+  def create
+    User.where(username: params[:user][:username], locu_str_id: params[:user][:locu_str_id]).first_or_create
   end
 
   def login
@@ -7,13 +8,9 @@ class WelcomeController < ApplicationController
     user = User.where(username: username)
     if user
       session['current_locu_id'] = user.locu_str_id
-
+      flash[:success] = "logged in with #{user.locu_str_id}"
     else
       redirect_to root_url, alert: "Couldn't find that username!"
     end
-  end
-
-  def signup
-    User.where(username: params[:username], locu_str_id:).first_or_create
   end
 end
