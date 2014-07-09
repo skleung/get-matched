59302480db26d5f99eafc2c99e60226f08190479
Kevin Casey <kcasey@godaddy.com>
2014-07-09 13:16:03 -0700

    finding nearby buisnesses

--- Gemfile
+++ Gemfile
@@ -1,7 +1,14 @@
 source 'https://rubygems.org'
 
 
-#gem 'annotate'
+gem 'annotate'
+gem "twitter-bootstrap-rails"
+
+group :test, :development do
+  gem 'byebug'
+  gem 'sqlite3'
+end
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.4'
 # Use sqlite3 as the database for Active Record
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
-  def searchForNearbyBuisness(locu_id, category = nil)
+  def searchForNearbyBuisness(locu_id, distance = 5000, category = nil)
+    api_key = "fb7523f94c32524215421f2a00ded5e01727b303"
     url = 'https://api.locu.com'
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
@@ -7,23 +8,37 @@ module ApplicationHelper
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Post.new("/v2/venue/search")
     data = {
-          api_key: "fb7523f94c32524215421f2a00ded5e01727b303",
-          fields: [ "name", "menu_items" ],
+          api_key: api_key,
+          fields: [ "name", "location", "contact" ],
           venue_queries: [
             {
-              location: {
-                locality: "Boston"
-              }
-            }
-          ],
-          menu_item_queries: [
-            {
-              name: "pizza"
+              locu_id: locu_id
             }
           ]
         }
     request.body = data.to_json
     request.add_field('Content-Type', 'application/json')
+    response = http.request(request)
+    results = response.body
+    json_results = JSON.parse(results)
+    buisness = json_results["venues"][0]
+    coordinate = buisness["location"]["geo"]["coordinates"]
+
+    data = {
+      api_key: api_key,
+      fields: [ "name", "location", "contact" ],
+      venue_queries: [
+        {
+          location: {
+            geo: {
+              "$in_lat_lng_radius" => [coordinate[1], coordinate[0], distance]
+            }
+          }
+        }
+      ]
+    }
+
+    request.body = data.to_json
     response = http.request(request)
     response.body
   end
