6873495a636f60edefe23fbf88f00cccd34828b8
Ivan Wang <iwang@godaddy.com>
2014-07-09 23:48:53 -0700

    Update search result distance for maximal cover photo goodness

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,7 +12,7 @@ class SearchController < ApplicationController
     return candidates
   end
 
-  def searchForNearbyBusinesses(distance=5000, category=nil)
+  def searchForNearbyBusinesses(distance=99999999, category=nil)
     business = searchForBusiness(session["current_locu_id"])
     coordinates = business["location"]["geo"]["coordinates"]
     fields = ["name", "location", "contact", "categories", "media"]
@@ -22,7 +22,8 @@ class SearchController < ApplicationController
           geo: {
             "$in_lat_lng_radius" => [coordinates[1], coordinates[0], distance]
           }
-        }
+        },
+        media: {"$present" => true}
       }
     ]
 
