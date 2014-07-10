983c48fadd36471d55c3178d01a33da24ff8c9c1
Ivan Wang <iwang@godaddy.com>
2014-07-10 09:57:29 -0700

    Migrate user + needs, categories

--- app/models/need.rb
+++ app/models/need.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: needs
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  str_id     :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Need < ActiveRecord::Base
 	has_and_belongs_to_many :users
 end
